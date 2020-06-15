02099f65747140e4b99f3887dcba7d5e5798ab98
cortezd334 <cortezd334@gmail.com>
2020-06-15 15:49:34 -0700

    Migration Successful

--- db/migrate/20200615185835_create_wine_clubs.rb
+++ db/migrate/20200615185835_create_wine_clubs.rb
@@ -1,9 +1,5 @@
 class CreateWineClubs < ActiveRecord::Migration[5.2]
-  has_many :customer_lists
-  has_many :customers, through: :customer_lists
 
-  has_many :wine_lists
-  has_many :wines, through: :wine_lists
 
   def change
     create_table :wine_clubs do |t|
--- db/migrate/20200615185915_create_customers.rb
+++ db/migrate/20200615185915_create_customers.rb
@@ -1,13 +1,12 @@
 class CreateCustomers < ActiveRecord::Migration[5.2]
-  has_many :customer_lists
-  has_many :wine_clubs, through: :customer_lists
+
 
   def change
     create_table :customers do |t|
       t.string :name
       t.integer :age
       t.string :wine_preference
-      t.sting :origin_preference
+      t.string :origin_preference
     end
   end
 end
